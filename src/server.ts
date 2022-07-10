0db969193e1ef010211d11fbcd01b8ff1480246a
Mordecai Godwin <davidire71@gmail.com>
2022-07-10 14:43:58 +0100

    Updated package.json, deployed to elastic beanstalk and added elastic beanstalk dashboard screenshort.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -10,7 +10,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,30 +30,30 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
-  app.get('/filteredImage',async (req:Request, res: Response) => {
+  app.get('/filteredImage', async (req: Request, res: Response) => {
     const image_url = req.query.image_url.toString();
-    if (!image_url) 
+    if (!image_url)
       res.status(400).send('Image url is required');
 
-      const filtered_image = await filterImageFromURL(image_url);
+    const filtered_image = await filterImageFromURL(image_url);
 
-      res.status(200).sendFile(filtered_image, () => {
-        deleteLocalFiles([filtered_image]);
-      });
+    res.status(200).sendFile(filtered_image, () => {
+      deleteLocalFiles([filtered_image]);
+    });
   });
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
