8c9b518e4152d55a985937ba445d588a93c95ac8
Mordecai Godwin <davidire71@gmail.com>
2022-07-10 14:10:44 +0100

    Added filter image endpoint to server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -28,6 +29,18 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+
+  app.get('/filteredImage',async (req:Request, res: Response) => {
+    const image_url = req.query.image_url.toString();
+    if (!image_url) 
+      res.status(400).send('Image url is required');
+
+      const filtered_image = await filterImageFromURL(image_url);
+
+      res.status(200).sendFile(filtered_image, () => {
+        deleteLocalFiles([filtered_image]);
+      });
+  });
 
   //! END @TODO1
   
